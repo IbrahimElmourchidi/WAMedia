c6c8e029e4f545781cddd24a2169105ae52149b5
ibrahimElmourchidi <ibrahimelmourchidi@gmail.com>
2023-01-14 10:22:13 +0200

    v1

--- API/Helpers/MessageHandler.cs
+++ API/Helpers/MessageHandler.cs
@@ -22,56 +22,101 @@ public class MessageHandler
 
         _logger = logger;
 
+        System.IO.Directory.CreateDirectory("files/images");
+        System.IO.Directory.CreateDirectory("files/videos");
+        System.IO.Directory.CreateDirectory("files/documents");
+        System.IO.Directory.CreateDirectory("files/audios");
     }
 
     public void HandleMessage(MessageObject message)
     {
         var messageType = message.entry[0].changes[0].value.messages[0].type;
         _logger.LogInformation(messageType);
+        string mimeType;
         switch (messageType)
         {
             case "image":
                 var imageId = message.entry[0].changes[0].value.messages[0].image.id;
-                downloadImage(imageId);
+                mimeType = message.entry[0].changes[0].value.messages[0].image.mime_type;
+                downloadImage(imageId, mimeType);
                 break;
             case "video":
                 var videoId = message.entry[0].changes[0].value.messages[0].video.id;
-                downloadVideo(videoId);
+                mimeType = message.entry[0].changes[0].value.messages[0].video.mime_type;
+                downloadVideo(videoId, mimeType);
+                break;
+            case "document":
+                var documentId = message.entry[0].changes[0].value.messages[0].document.id;
+                mimeType = message.entry[0].changes[0].value.messages[0].document.mime_type;
+                downloadDocument(documentId, mimeType);
+                break;
+            case "audio":
+                var audioId = message.entry[0].changes[0].value.messages[0].audio.id;
+                mimeType = message.entry[0].changes[0].value.messages[0].audio.mime_type;
+                downloadAudio(audioId, mimeType);
                 break;
 
-            
+
         };
     }
 
-    private async Task<bool> downloadVideo(string videoId)
+    private async Task<bool> downloadVideo(string videoId, string mimeType)
     {
-        _logger.LogInformation("downloading video ...");
+        var extension = mimeType.Split('/')[1];
+        _logger.LogInformation(extension);
         var mediaUrl = await GetMediaUrl(videoId);
         if (mediaUrl != null)
         {
             byte[] fileBytes = await _httpClient.GetByteArrayAsync(mediaUrl);
-            await File.WriteAllBytesAsync("files/test.mp4", fileBytes);
-            _logger.LogInformation("saved video successfully");
+            await File.WriteAllBytesAsync($"files/videos/{videoId}.{extension}", fileBytes);
             return true;
         }
         _logger.LogError("cannot video  locally");
         return false;
     }
 
-    private async Task<bool> downloadImage(string imageId)
+    private async Task<bool> downloadImage(string imageId, string mimeType)
     {
-
-
-        _logger.LogInformation("downloading image ...");
+        var extension = mimeType.Split('/')[1];
+        _logger.LogInformation(extension);
         var mediaUrl = await GetMediaUrl(imageId);
         if (mediaUrl != null)
         {
             byte[] fileBytes = await _httpClient.GetByteArrayAsync(mediaUrl);
-            await File.WriteAllBytesAsync("files/test.jpg", fileBytes);
-            _logger.LogInformation("saved image successfully");
+            await File.WriteAllBytesAsync($"files/images/{imageId}.{extension}", fileBytes);
             return true;
         }
         _logger.LogError("cannot save image locally");
+        return false;
+    }
+
+    private async Task<bool> downloadAudio(string audioId, string mimeType)
+    {
+        var extension = mimeType.Split('/')[1].Split(';')[0];
+        _logger.LogInformation(extension);
+        var mediaUrl = await GetMediaUrl(audioId);
+        if (mediaUrl != null)
+        {
+            byte[] fileBytes = await _httpClient.GetByteArrayAsync(mediaUrl);
+            await File.WriteAllBytesAsync($"files/audios/{audioId}.{extension}", fileBytes);
+            return true;
+        }
+        _logger.LogError("cannot save audio locally");
+        return false;
+    }
+
+    private async Task<bool> downloadDocument(string documentId, string mimeType)
+    {
+        var extension = mimeType.Split('/')[1];
+        _logger.LogInformation(mimeType);
+        var mediaUrl = await GetMediaUrl(documentId);
+        if (mediaUrl != null)
+        {
+            byte[] fileBytes = await _httpClient.GetByteArrayAsync(mediaUrl);
+            await File.WriteAllBytesAsync($"files/documents/{documentId}.{extension}", fileBytes);
+            return true;
+        }
+        _logger.LogError("cannot save document locally");
         return false;
     }
 
